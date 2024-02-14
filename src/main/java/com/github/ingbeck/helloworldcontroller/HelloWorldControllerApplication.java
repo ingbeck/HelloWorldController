1b75f5d9f4f027812e0eb367842026393175f571
ingobecker <mail@ingobecker.de>
2024-02-14 11:14:42 +0100

    initial commit

---
+++ src/main/java/com/github/ingbeck/helloworldcontroller/HelloWorldControllerApplication.java
@@ -0,0 +1,13 @@
+package com.github.ingbeck.helloworldcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ingbeck/helloworldcontroller/HelloWorldControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ingbeck.helloworldcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
