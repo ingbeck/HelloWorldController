be5f5072d0bc01541987f27247208400b1b47d0f
ingobecker <mail@ingobecker.de>
2024-02-14 11:50:25 +0100

    add method to POST and GET messages

--- src/main/java/com/github/ingbeck/helloworldcontroller/HelloWorldController.java
+++ src/main/java/com/github/ingbeck/helloworldcontroller/HelloWorldController.java
@@ -1,26 +1,42 @@
 package com.github.ingbeck.helloworldcontroller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/hello")
+@RequestMapping("/api/message")
 public class HelloWorldController {
 
 
+    private List<Message> messages = new ArrayList<>();
 
-    @GetMapping
+    public HelloWorldController(){
+        messages.add(new Message("System", "Hier kommt deine Message an", "1"));
+    }
+
+    @GetMapping("/hello")
     public String getHello(){
         return "HelloWorld";
     }
     //Output: 1 HelloWorld
 
-    @GetMapping("/{name}")
+    @GetMapping("/hello/{name}")
     public String getHello(@PathVariable String name){
         return "Hello, " + name;
     }//Output: 1 Hello, ingo
+
+    @GetMapping()
+    public List<Message> getMessages(){
+        return messages;
+    }
+
+    @PostMapping()
+    public List<Message> addMessage(@RequestBody Message message){
+        messages.add(message);
+        return messages;
+    }
 
 }
